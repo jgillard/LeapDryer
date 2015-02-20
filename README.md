7ae309422246f4f597aa6e12470c51c44e7953d3
James Gillard <jamesgillard@live.co.uk>
2015-02-20 11:29:34 +0000

    Detect Arduino serial port

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Leap Dryer
 
-## 4th Year MEng Engineering Project
-## School of Engineering, University of Warwick
+### 4th Year MEng Engineering Project
+### School of Engineering, University of Warwick
 
 Using a Leap Motion to control the air flow velocity of a hand dryer with RC servos.
 
--- servosendval.py
+++ servosendval.py
@@ -1,10 +1,18 @@
+from serial.tools import list_ports
+from time import sleep 
 import serial
-from time import sleep
-s = serial.Serial('/dev/tty.usbmodemfa131', 9600)
+
+port = [s for s in zip(*list_ports.comports())[0] if 'iPhone' in s]
+
+if not port:
+	print "Arduino not detected"
+else:
+	s  = serial.Serial(port[0], 9600)
+
 sleep(2)
 s.write('0\n')
 
 while True: 
-	degrees = raw_input("Gimmee numberz: ")
+	degrees = raw_input("Enter degree value: ")
 	s.write(degrees)
 	s.write('\n')
