4216f0dcd6a2e9f4b1034f87ef0f60ecc92bfda7
James Gillard <jamesgillard@live.co.uk>
2015-04-16 14:50:33 +0100

    Add LED feedback, equations from list values

--- servoUnoIR/servoUnoIR.ino
+++ servoUnoIR/servoUnoIR.ino
@@ -1,12 +1,19 @@
 #include <Servo.h>
-#include <QueueArray.h>
 
 int nozzlePin = 6;
 int motorPin = 9;
-int IRpin = 0;
+int IRpin = 5;
+int redLED = 3;
+int yellowLED = 10;
+int greenLED = 11;
 Servo nozzleServo;
 Servo motorServo;
 int queue[5];
+
+int nozzleMaxMin[] = {110, 80};
+int motorMaxMin[] = {90, 1};
+//int farVal = 350;
+//int closeVal = 600;
 
 void setup() {
   Serial.begin(115200);
@@ -14,30 +21,45 @@ void setup() {
   motorServo.attach(motorPin);
   nozzleServo.write(120);
   motorServo.write(40);
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+//  digitalWrite(redLED, HIGH);
+//  digitalWrite(yellowLED, HIGH);
+//  digitalWrite(greenLED, HIGH);
+  startupLEDs();
+
+  // m = (nozzleMaxMin[0] - nozzleMaxMin[1]) / (farVal - closeVal);
+}
+
+void startupLEDs() {
+  digitalWrite(redLED, HIGH);
+  delay(1000);
+  digitalWrite(yellowLED, HIGH);
+  delay(1000);
+  digitalWrite(greenLED, HIGH);
+
 }
 
 void loop() {
   int dist = analogRead(IRpin);
   int sum = 0;
   int avg = 0;
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 4; i++) {
     queue[i] = queue[i+1];
     sum += queue[i];
   }
-  queue[4] = dist;
-  sum += queue[4];
-  avg = sum / 5;
+  queue[3] = dist;
+  sum += queue[3];
+  avg = sum / 4;
     
   Serial.print("Dist: ");
   Serial.print(dist);
   Serial.print(" Avg: ");
   Serial.print(avg);
   
-  int nozzleVal = -0.1 * avg + 130;
-  int motorVal = 0.26 * avg - 52;
-  
-  int nozzleMaxMin[] = {110, 80};
-  int motorMaxMin[] = {90, 1};
+  int nozzleVal = -0.12 * dist + 152;
+  int motorVal = 0.356 * dist - 124;
   
   if (nozzleVal > nozzleMaxMin[0]) { nozzleVal = nozzleMaxMin[0]; }
   else if (nozzleVal < nozzleMaxMin[1]) { nozzleVal = nozzleMaxMin[1]; }
@@ -51,4 +73,11 @@ void loop() {
   
   nozzleServo.write(nozzleVal);
   motorServo.write(motorVal);
+  
+  if (dist > 500) { digitalWrite(yellowLED, LOW); }
+  else { digitalWrite(yellowLED, HIGH); }
+  if (dist > 350) { digitalWrite(greenLED, LOW); }
+  else { digitalWrite(greenLED, HIGH); }
+  
+ 
 }
